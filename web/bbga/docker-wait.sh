72767d3ef44d7c34ef21b1ab838bd4f14f917424
Stephan Preeker <stephan@preeker.net>
2016-04-14 10:54:51 +0200

    ATLAS-1516 wip verdockeren

--- web/bbga/docker-wait.sh
+++ web/bbga/docker-wait.sh
@@ -4,7 +4,7 @@ set -u
 set -e
 
 # wait for postgres
-while ! nc -z ${PARKEERVAKKEN_DB_PORT_5432_TCP_ADDR} ${PARKEERVAKKEN_DB_PORT_5432_TCP_PORT}
+while ! nc -z ${BBGA_DB_PORT_5432_TCP_ADDR} ${BBGA_DB_PORT_5432_TCP_PORT}
 do
 	echo "Waiting for postgres..."
 	sleep 0.1
