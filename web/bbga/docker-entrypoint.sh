901b31720df77dd9e2d66b675863e48645be2fb7
Yigal Duppen <yigal@publysher.nl>
2016-04-18 10:49:04 +0200

    Opschonen Dockerfiles

--- web/bbga/docker-entrypoint.sh
+++ web/bbga/docker-entrypoint.sh
@@ -11,16 +11,5 @@ python manage.py collectstatic --noinput
 # migrate database tables
 yes yes | python manage.py migrate --noinput
 
-# run import
-#python manage.py run_import
-
-#python manage.py run_import /app/data/bbga_csv/metadata_utf8.csv  bbga_data_meta
-
-#python manage.py run_import /app/data/bbga_csv/bbga_tableau
-#
-
-# sync geo views
-# python manage.py sync_views
-
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
